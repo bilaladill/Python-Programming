1fe427e5f8fdf2f8471ea7566b4c70d847516e10
bilaladill <bilaladilhameed@gmail.com>
2022-10-20 21:37:06 +0500

    bilal programming

--- 6. Logic Building Practice.py
+++ 6. Logic Building Practice.py
@@ -6,3 +6,37 @@
 # less             <
 # greater or equal >=
 # less or equal    <=
+
+# ============================= Programs ===============================
+
+# =============== If, Elif, Else =================
+
+import math
+
+name = str(input('Enter your name: '))
+age = int(input('Enter your age: '))
+rank = int(input('Enter your class: '))
+section = str(input('Enter your section: '))
+subject = str(input('Enter learning subject: '))
+total_marks = int(input('Enter total marks: '))
+obtained_marks = int(input('Enter obtained marks: '))
+percentage = math.floor((obtained_marks/total_marks)*100)
+# grade = str('A', 'A+', 'B', 'C', )
+
+
+if percentage >= 80 and percentage <= 100:
+    print('Grade: A+')
+#
+elif percentage >= 70 and percentage < 80:
+    print('Grade: A')
+#
+elif percentage >= 60 and percentage < 70:
+    print('Grade : B')
+#
+elif percentage >= 50 and percentage < 60:
+    print('Grade: C')
+#
+else:
+    print('Grade: Failed')
+
+print(f'--- THE DECENCY CODIFIED MARKETING ---\nName: {name}\nAge: {age}\nClass: {rank}\nSection: {section}\nSubject: {subject}\nObtained Marks: {obtained_marks}\nTotal Marks: {total_marks}\nPercentage: {percentage}%\n--- RESULT FROM THE DECENCY CODIFIED MARKETING ---')
---
+++ 7. Functions.py
@@ -0,0 +1,93 @@
+# ================= Functions ===========================
+'''def myFunction():
+    pass
+
+
+myFunction() 
+print(myFunction)
+print(myFunction())'''
+
+
+'''def myFunc():  # defining a function
+    print('Function has been Executed!!')
+
+
+# Calling function is very important to execute a function
+myFunc()'''
+
+# arguments in Function
+
+
+'''def Function1(name, food):
+    print(name + '! your Favorite food is ' + food)
+
+
+Function1('Haris', 'French Fries')'''
+
+
+'''def add(a, b):
+    print(a*b)
+
+
+add(5, 9)'''
+
+# parameters in functions
+
+
+'''def addition(a=10, b=10):
+    print(a+b)
+
+
+addition()
+addition(55, 66)
+addition(55, )
+addition(66)'''
+
+# number of Arguments
+
+
+'''def name(fname='amin', mname='N/A', lname='N/A', education='N/A'):
+    print(f'Welcome {fname} {mname} {lname}! you are a {education}')
+
+
+name()
+name('Amin', 'Ahmed', 'Zuberi', 'Professional of Cyber Security')'''
+# Arbitrary Arguments, *args (tuple)
+
+
+'''def ArbArg(*classmates):
+    print('The Smartest Boys are ' + classmates[0] + ' & ' + classmates[0])
+
+
+ArbArg('Haris', 'Amin', 'Bilal', 'Sami', 'Farhan')'''
+
+# Arbitrary kewords arguments, **kwargs (Dictionary)
+
+
+'''def myCars(**cars):
+    print('The 1st Best Car is '+cars['car1'])
+    print('The 2nd Best Car is '+cars['car2'])
+    print('The 3rd Best Car is '+cars['car3'])
+
+
+myCars(car1='BMW', car2='Audi', car3='Tesla')'''
+# functions with list
+'''cars = ['BMW', 'Audi', 'Tesla']
+
+
+def myCars(CARS):
+    for car in CARS:
+        print(car)
+
+
+myCars(cars)'''
+
+# Return
+
+
+def func(arg):
+    return print(5*arg)
+
+
+func('Amin ')
+func(14)
