6aafdf3c3d182ed98f350347aa9aff4e0298bc06
bilaladill <bilaladilhameed@gmail.com>
2022-10-25 20:16:04 +0500

    bilal programming

--- 8. Functions Practice.py
+++ 8. Functions Practice.py
@@ -154,23 +154,23 @@
 # ====================
 
 
-# def names(fname, lname):
-#     fullName = f'{fname} {lname}'
-#     return fullName.title()
+'''def names(fname, lname):
+    fullName = f'{fname} {lname}'
+    return fullName.title()
 
 
-# while True:
-#     print("Enter 'q' to quit the program.")
-#     print('Please enter your name below.')
-#     FName = input('Enter your first name: ')
-#     if FName == 'q':
-#         break
-#     LName = input('Enter your last name: ')
-#     if LName == 'q':
-#         break
-#     Name = names(FName, LName)
-#     print(f'Hello {Name} how are you?')
-
+while True:
+    print("Enter 'q' to quit the program.")
+    print('Please enter your name below.')
+    FName = input('Enter your first name: ')
+    if FName == 'q':
+        break
+    LName = input('Enter your last name: ')
+    if LName == 'q':
+        break
+    Name = names(FName, LName)
+    print(f'Hello {Name} how are you?')
+'''
 # ========================
 
 
