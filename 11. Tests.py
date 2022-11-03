92c8d80d4730ff31d4758ac3af2bca40021eb169
bilaladill <bilaladilhameed@gmail.com>
2022-11-03 17:06:05 +0500

    bilal programming

--- 11. Tests.py
+++ 11. Tests.py
@@ -1,1 +1,22 @@
 # ================= 3-Nov-22 =================
+
+# =========== 10/10 given by sir firhan ===========
+
+# class Players:
+#
+#     Total_Players = 11
+#
+#     def __init__(self, fname, lname, type, order):
+#         self.fname = fname
+#         self.lname = lname
+#         self.type = type
+#         self.order = order
+#
+#     def __str__(self):
+#         return f'Name: {self.fname} {self.lname}\nType: {self.type}\nOrder: {self.order}'
+
+
+# player = Players('Haris', 'Rauf', 'Fast Bowler', 9)
+# print(player)
+# Total_Players = f'Total Players: {Players.Total_Players}'
+# print(Total_Players)
