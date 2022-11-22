66644294fd1a40120d660e6f7c05cc166c0c7db0
bilaladill <bilaladilhameed@gmail.com>
2022-11-22 12:08:53 +0500

    bilal programming

--- 2a. Programs.py
+++ 2a. Programs.py
@@ -9,7 +9,7 @@
 # percentage = math.floor((obtained_marks/total_marks)*100)
 # print(f'Your name is {name} and your age is {age} years. Your id is {id}. Your total marks were {total_mrks} and you have obtained {obtained_marks} marks. Which becomes {percentage}%.')
 
-# ============================== List ======================================
+# ============ List =============
 
 
 # my_list = ['Amin', 'Farhan', 'Haris', 'Bilal']
@@ -18,84 +18,85 @@
 # ============= 28 - october - 2022 ================
 
 
-'''def StudentProfile(fname, lname, **StudentInfo):
-    StudentInfo['First Name'] = fname
-    StudentInfo['Last Name'] = lname
-    return StudentInfo
+# def StudentProfile(fname, lname, **StudentInfo):
+#     StudentInfo['First Name'] = fname
+#     StudentInfo['Last Name'] = lname
+#     return StudentInfo
 
+# Std_profile = StudentProfile('Haris', 'Ahmed', percentage='51%')
+# print(Std_profile)
+# 
+# StdFatherName = str(input('Enter your Father Name: '))
+# StdClass = int(input('Enter your class: '))
+# print(
+#     f'Student Name: {Std_profile}!\nFater Name: {StdFatherName}\nStudent Class: {StdClass}')
+ 
 
-Std_profile = StudentProfile('Haris', 'Ahmed', percentage='51%')
-print(Std_profile)
-
-StdFatherName = str(input('Enter your Father Name: '))
-StdClass = int(input('Enter your class: '))
-print(
-    f'Student Name: {Std_profile}!\nFater Name: {StdFatherName}\nStudent Class: {StdClass}')
-'''
-
+# =========================
 
 # Make a programe using strings., function., class, Interger., input., for loop
+
 # import math
-#
-#
+
 # class Bilal:
-#
+
 #     obtainedMarks = input('Obtaied Marks out of 750: ')
 #
 #     def __init__(self, fname, lname, sports):
 #         self.fname = fname
 #         self.lname = lname
 #         self.sports = sports
-#
+
 #     def __str__(self):
 #         return f'First Name: {self.fname}\nLast Name: {self.lname}\nFavorite Sports: {self.sports}'
-#
+
 #     def myinfo():
 #         percentage = math.floor((int(Bilal.obtainedMarks)/750)*100)
 #         return f'You have Result is as Follow,\nResult: {percentage}%'
-#
-#
+
 # myInformation = Bilal('Bilal', 'Adil', 'Cricket')
 # print(myInformation)
 # print(Bilal.myinfo())
 
-# ========= 19 - Nov - 22 ===========
+# ===================== 19 - Nov - 22 ======================
 
 # Sample Programe by Sir Firhan
 
-'''import math
-numberofStudents, totalMarks, studentName, studentAge, studentClass, studentMarks = 5, 500, [], [], [], []
+# import math
+# numberofStudents, totalMarks, studentName, studentAge, studentClass, studentMarks = 5, 500, [], [], [], []
+# 
+# for student in range(1, numberofStudents):
+#     global name, age, grade, marks
+# 
+#     name = input('Enter Your name: ')
+#     age = input('Enter Your Age: ')
+#     grade = input('Enter Your Class: ')
+#     marks = int(input('Enter Your Marks: '))
+# 
+#     studentName.append(name)
+#     studentAge.append(age)
+#     studentClass.append(grade)
+#     studentMarks.append(marks)
+# 
+# print(studentName, studentAge, studentClass, studentMarks)
+# 
+# 
+# def result():
+#     return print(f'student Name: {studentName[index]}\nstudent Age:{studentAge[index]}\nstudent Grade:{studentClass[index]} \nObtained Marks:{studentMarks[index]}\nPercentage:{math.floor((studentMarks[index]/totalMarks)*100)}%\ncomment: Congrats! You Got First Position!!')
+# 
+# 
+# if studentMarks[0] > (studentMarks[1] and studentMarks[2] and studentMarks[3]):
+#     index = 0
+#     result()
+# elif studentMarks[1] > (studentMarks[0] and studentMarks[2] and studentMarks[3]):
+#     index = 1
+#     result()
+# elif studentMarks[2] > (studentMarks[1] and studentMarks[1] and studentMarks[3]):
+#     index = 2
+#     result()
+# else:
+#     index = 3
+#     result()
 
-for student in range(1, numberofStudents):
-    global name, age, grade, marks
+# ========================
 
-    name = input('Enter Your name: ')
-    age = input('Enter Your Age: ')
-    grade = input('Enter Your Class: ')
-    marks = int(input('Enter Your Marks: '))
-
-    studentName.append(name)
-    studentAge.append(age)
-    studentClass.append(grade)
-    studentMarks.append(marks)
-
-print(studentName, studentAge, studentClass, studentMarks)
-
-
-def result():
-    return print(f'student Name: {studentName[index]}\nstudent Age:{studentAge[index]}\nstudent Grade:{studentClass[index]} \nObtained Marks:{studentMarks[index]}\nPercentage:{math.floor((studentMarks[index]/totalMarks)*100)}%\ncomment: Congrats! You Got First Position!!')
-
-
-if studentMarks[0] > (studentMarks[1] and studentMarks[2] and studentMarks[3]):
-    index = 0
-    result()
-elif studentMarks[1] > (studentMarks[0] and studentMarks[2] and studentMarks[3]):
-    index = 1
-    result()
-elif studentMarks[2] > (studentMarks[1] and studentMarks[1] and studentMarks[3]):
-    index = 2
-    result()
-else:
-    index = 3
-    result()
-'''
