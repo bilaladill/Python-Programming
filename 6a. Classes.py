f5aa8c07ed18b31ea0336727ba286538368b3f5c
bilaladill <bilaladilhameed@gmail.com>
2022-12-01 18:01:35 +0500

    bilal programming

--- 6a. Classes.py
+++ 6a. Classes.py
@@ -498,3 +498,102 @@ print(emp1.bioData())
 #         print('there is no error!')
 # except:
 #     print('executed')
+
+# =========== File Handling ===================
+
+# f = open('haris.txt')
+# print(f)
+# print(f.read(5))
+# h = open('G:\\bilal.txt', 'rt')
+# print(h.read(10))
+# print(h.read(5))
+
+# ===========
+
+# d = open('haris.txt')
+# print(d.readline())
+# print(d.readline())
+# print(d.readline())
+
+# ===========
+
+# a = open('haris.txt')
+# for x in a:
+#     print(x)
+
+# ===========
+
+# b = open('G:\\bilal.txt')
+# print(b.readline())
+# b.close()
+
+#============== Writing to an Existing File ====================
+'''a = open('abc.txt','a')
+a.write('they both tease each other!')
+a.close()
+
+a = open('abc.txt','r')
+print(a.read())'''
+
+'''b = open('abc.txt','w')
+b.write('this is a file named as abc')
+b.close()
+
+b = open('abc.txt','r')
+print(b.read())
+'''
+
+# creating a new file xyz
+'''c = open('xyz.txt','x')
+c.close()'''
+
+# d = open('xyz.txt','a')
+# d.write('''haris is crying!
+# uwain uwain uwain!!!
+# he is also good boy''')
+# d.close()
+
+'''d = open('xyz.txt','r')
+print(d.read())'''
+
+#=============== Deleting files =============
+# import os
+# os.remove('xyz.txt')
+
+'''if os.path.exists('xyz.txt'):
+    os.remove('xyz.txt')
+else:
+    print('there is no such file!!')'''
+    
+
+#=============== JSON Files ========================
+import json
+'''# Json to python
+x = '{"name":"Bilal", "Age":30,"city":"Karachi"}'
+y = json.loads(x)
+print(y)
+print(y.keys())
+print(y.values())
+print(x)
+'''
+
+mydict = {
+    'name':'haris',
+    'age':16,
+    'city':'california'
+    
+}
+z = json.dumps(mydict)
+print(z)
+
+# we can convert following python objects into JSON i.e.
+# dict,list,tuple,string,int,float,True,False,None
+print(json.dumps({"name":"Bilal", "Age":30,"city":"Karachi"}))
+print(json.dumps(["Bilal", 30,"Karachi"]))
+print(json.dumps(("Bilal", 30,"Karachi")))
+print(json.dumps("Bilal"))
+print(json.dumps(55))
+print(json.dumps(55.55))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
