f194edbb6a77be1b320a4c430555317b8d91d477
bilaladill <bilaladilhameed@gmail.com>
2022-12-02 17:22:03 +0500

    bilal programming

---
+++ 7a. Scope.py
@@ -0,0 +1,31 @@
+# =================== Scope ======================
+
+# === (IT IS USED IN WHOLE PYTHON) ===
+
+# def bilal():
+#     global b
+#     a = 5
+#     b = 9
+#     print(a)
+#     print(b*4)
+# 
+# bilal()
+# print(b)
+
+# =================== 
+
+# def results():
+#     global percentage,obt,tot,database
+#     database = []
+#     obt = int(input('Obtained Marks'))
+#     tot = int(input('Total Marks'))
+#     percentage = obt/tot*100
+#     database.append(percentage)
+
+# print(percentage)
+
+# results()
+
+# print(percentage)
+# print(obt)
+# print(database)
---
+++ 8a. Modues.py
@@ -0,0 +1,28 @@
+# ================== Modules  ====================
+
+# === (IT IS USED IN WHOLE PYTHON) ===
+
+# import math
+# def sin(a):
+#     return print(math.sin(a))
+# 
+# sin(30)
+# import Modules
+# Modules.sin(100)
+# print(Modules.mydict)
+# print(Modules.mydict.items())
+
+# =========
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x)
+# print(x.year)
+# print(x.hour)
+# print(x.day)
+# print(x.month)
+# print(x.minute)
+# print(x.second)
+# print(x)
+
+# import numpy  as np
