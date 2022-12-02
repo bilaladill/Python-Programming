744a96c86d2ea1ad0466514ae32825995e587aec
bilaladill <bilaladilhameed@gmail.com>
2022-12-02 18:19:54 +0500

    bilal programming

--- 11a. JSON Fies.py
+++ 11a. JSON Fies.py
@@ -5,7 +5,7 @@
 # import json
 
 # x = '{"name":"Bilal", "Age":30,"city":"Karachi"}'
-# y = json.loads(x)
+# y = json.loads(x) # json to python
 # print(y)
 # print(y.keys())
 # print(y.values())
@@ -17,7 +17,7 @@
 #     'city':'california'
 #     
 # }
-# z = json.dumps(mydict)
+# z = json.dumps(mydict) # python to json
 # print(z)
 
 # we can convert following python objects into JSON i.e.
@@ -32,3 +32,22 @@
 # print(json.dumps(True))
 # print(json.dumps(False))
 # print(json.dumps(None))
+
+'''import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+
+print(json.dumps(x))
+print(json.dumps(x, indent=4))'''
+
---
+++ 12a. RegEx.py
@@ -0,0 +1,12 @@
+import re
+
+sentence = 'We are studying about the regular Expressions'
+a = re.search('^We.*Expressions$',sentence)
+if a:
+    print('yes, we have match')
+else:
+    print('no match')
+b = re.findall('r',sentence)
+print(b)
+c = re.split('\s',sentence)
+print(c)
